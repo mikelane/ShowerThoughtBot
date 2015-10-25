b962010c1f497537947429b4b3a052044cdc426f
Mike Lane <mikelane@gmail.com>
2015-10-25 00:07:45 -0700

    Makes bot more general for re-use. Adds channel and nick sniffing.

--- ShowerThoughtBot.py
+++ ShowerThoughtBot.py
@@ -10,24 +10,30 @@ __author__ = 'Mike Lane (http://www.github.com/mikelane/)'
 __copyright__ = 'Copyright (c) 2015 Mike Lane'
 __license__ = 'GPLv3'
 
-import socket, ssl, yaml, time
+import socket, ssl, yaml, time, re
 
-class ShowerThoughtBot:
+class Bot:
     def __init__(self):
         with open('config.yml', 'r') as y:
-            self.c = yaml.load(y)
+            config = yaml.load(y)
+            self.server = config['server']
+            self.port = int(config['port'])
+            self.nick = config['nick']
+            self.channels = config['channels']
             self.s = socket.socket()
-            self.s.connect((self.c['server'], self.c['port']))
+            self.s.connect((self.server, self.port))
             self.ircsock = ssl.wrap_socket(self.s)
+            self.ircsock.send("USER {} {} {} :{}\r\n".format(self.nick,
+                                                             self.nick,
+                                                             self.nick,
+                                                             self.nick).encode())
+            self.ircsock.send("NICK {}\r\n".format(self.nick).encode())
             time.sleep(2)
-            self.ircsock.send("USER {} {} {} :{}\r\n".format(self.c['nick'],
-                                                             self.c['nick'],
-                                                             self.c['nick'],
-                                                             self.c['nick']).encode())
-            self.ircsock.send("NICK {}\r\n".format(self.c['nick']).encode())
+            for channel in self.channels:
+                self.joinchan(channel['name'], channel['key'])
 
-    def joinchan(self, chan):
-        self.ircsock.send("JOIN {} {}\r\n".format(chan, self.c['key']).encode())
+    def joinchan(self, chan, key):
+        self.ircsock.send("JOIN {} {}\r\n".format(chan, key).encode())
 
     def ping(self):
         self.ircsock.send("PONG :pingis\r\n".encode())
@@ -35,22 +41,36 @@ class ShowerThoughtBot:
     def sendmsg(self, chan, msg):
         self.ircsock.send("PRIVMSG {} :{}\r\n".format(chan, msg).encode())
 
-    def hello(self):
-        self.ircsock.send("PRIVMSG {} :Hello!\r\n".format(self.c['channel']).encode())
+    def hello(self, channel, nick):
+        self.ircsock.send("PRIVMSG {} :Hello, {}!\r\n".format(channel, nick).encode())
 
-bot = ShowerThoughtBot()
-time.sleep(2)
-bot.joinchan(bot.c['channel'])
+bot = Bot()
 
-while 1:
+while True:
+    # Gather input
     msg = bot.ircsock.recv(2048).decode()
+    # Strip newlines
     msg = msg.strip('\n\r')
+    # Determine what channel the input is from
+    chan = re.search('(\#\w+ )', msg)
+    if chan:
+        chan = chan.group(1)
+        # print("chan: {}".format(chan))
+    # Determine what user sent the message
+    fromNick = re.search('(\:\w+\!)', msg)
+    if fromNick:
+        fromNick = fromNick.group(1)
+        fromNick = fromNick.strip(':!')
+        # print("fromNick: {}".format(fromNick))
+    # If the message isn't empty, log it to the screen
     if(msg != ""):
         print(msg)
 
+    # If we get a ping, print the ping and execute the ping function
     if msg.find("PING :") != -1:
         print(msg)
         bot.ping()
 
-    if msg.find(":hello {}".format(bot.c['nick'])) != -1:
-        bot.hello()
+
+    if msg.find(":hello {}".format(bot.nick)) != -1:
+        bot.hello(chan, fromNick)
