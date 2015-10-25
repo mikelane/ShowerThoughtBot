c88307f12e48bc5e88fb9413411ffd007456d123
Mike Lane <mikelane@gmail.com>
2015-10-25 01:17:17 -0700

    Adds helpful comments.

--- ShowerThoughtBot.py
+++ ShowerThoughtBot.py
@@ -15,11 +15,16 @@ import socket, ssl, yaml, time, re
 class Bot:
     def __init__(self, file):
         with open(file, 'r') as y:
+            # Load the configs
             config = yaml.load(y)
-            self.server = config['server']
-            self.port = int(config['port'])
-            self.nick = config['nick']
+
+            # Store the configs in the Bot
+            self.server   = config['server']
+            self.port     = int(config['port'])
+            self.nick     = config['nick']
             self.channels = config['channels']
+
+            # Connect to the server
             self.s = socket.socket()
             self.s.connect((self.server, self.port))
             self.ircsock = ssl.wrap_socket(self.s)
@@ -28,7 +33,11 @@ class Bot:
                                                              self.nick,
                                                              self.nick).encode())
             self.ircsock.send("NICK {}\r\n".format(self.nick).encode())
+
+            # Have to sleep for a couple of potatoes so you don't try to join too early
             time.sleep(2)
+
+            # Join all the channels specified in the config file
             for channel in self.channels:
                 self.joinchan(channel['name'], channel['key'])
 
@@ -44,10 +53,12 @@ class Bot:
     def hello(self, channel, nick):
         self.ircsock.send("PRIVMSG {} :Hello, {}!\r\n".format(channel, nick).encode())
 
+# Initialize a bot!
 bot = Bot('config.yml')
 
+# Read, Evaluate, Print, Loop
 while True:
-    # Gather input
+    # Gather some input
     msg = bot.ircsock.recv(2048).decode()
     # Strip newlines
     msg = msg.strip('\n\r')
@@ -63,10 +74,11 @@ while True:
         fromNick = fromNick.strip(':!')
         # print("fromNick: {}".format(fromNick))
     # If the message isn't empty, log it to the screen
+    # @todo make this log to a file, too. Write a log function that does both
     if(msg != ""):
         print(msg)
 
-    # If we get a ping, print the ping and execute the ping function
+    # If we get a ping, log the ping and execute the ping function
     if msg.find("PING :") != -1:
         print(msg)
         bot.ping()
