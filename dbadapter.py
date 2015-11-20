d1c0d21d8a9acbdbf92366f4d4b971d096009b13
Michael Lane <mikelane@gmail.com>
2015-11-20 09:41:44 -0800

    Uses with keyword for better file handling.

--- dbadapter.py
+++ dbadapter.py
@@ -20,10 +20,10 @@ class DBAdapter:
     def __init__(self, file):
         self.file = file
         self.create_database()
-        f = open('vulgarities.txt', 'r')
         self.vulgarities = set()
-        for line in f:
-            self.vulgarities.add(line.rstrip())
+        with open('vulgarities.txt', 'r') as f:
+            for line in f:
+                self.vulgarities.add(line.rstrip())
 
     def create_database(self):
         with DBManager(self.file) as c:
